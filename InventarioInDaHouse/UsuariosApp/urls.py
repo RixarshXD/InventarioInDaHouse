8d9718d8bb37299fb02e38baf39b3632416837a0
Michael López <130867148+sq1fn@users.noreply.github.com>
2024-11-02 05:20:46 -0300

    se crea urls para Usuario

---
+++ InventarioInDaHouse/UsuariosApp/urls.py
@@ -0,0 +1,9 @@
+from django import path
+from .views import listado_usuarios, registrar_usuario, modificar_usuario, eliminar_usuario
+
+urlpatterns = [
+    path('', listado_usuarios, name='listado'),
+    path('registrar/', registrar_usuario, name='registrar'),
+    path('modificar/<int:id>/', modificar_usuario, name='modificar'),
+    path('eliminar/<int:id>/', eliminar_usuario, name='eliminar'),
+]
