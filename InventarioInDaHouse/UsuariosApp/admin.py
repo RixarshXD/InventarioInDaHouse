15f4d2d33ed3966659c98fdda3706281116ccd7b
RixarshXD <166057490+RixarshXD@users.noreply.github.com>
2024-11-04 20:25:50 -0300

    correcion de elemento eliminado de admin

--- InventarioInDaHouse/UsuariosApp/admin.py
+++ InventarioInDaHouse/UsuariosApp/admin.py
@@ -4,7 +4,7 @@ from .models import Usuario
 # Register your models here.
 
 class UsuarioAdmin(admin.ModelAdmin):
-    list_display = ['nombre', 'apellido', 'rut', 'correo', 'contrasena','telefono', 'fecha_nacimiento', 'tipo_usuario']
+    list_display = ['nombre', 'apellido', 'rut', 'correo', 'contrasena', 'fecha_nacimiento', 'tipo_usuario']
     search_fields = ['nombre', 'rut', 'tipo_usuario']
     
 admin.site.register(Usuario, UsuarioAdmin)
